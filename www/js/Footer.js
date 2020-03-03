034be483228b667c80d0f52101b53695069e5e04
DouglasKarlberg <DouglasKarlberg@protonmail.com>
2020-03-03 14:22:14 +0100

    Back To Top button added in footer

--- www/js/Footer.js
+++ www/js/Footer.js
@@ -1,11 +1,24 @@
 class Footer extends Base {
 
+  backToTop(){      //backToTop function is created
+    document.body.scrollTop = 0;              //Scroll to the top for Safari
+    document.documentElement.scrollTop = 0;   //Scroll to the top for Chrome, Firefox, IE, and Opera
+  }                 //backToTop function ends
+
   render() {
     return /*html*/`
       <footer class="bg-primary p-4 container-fluid">
         <div class="row">
-          <div class="col text-center text-light">
-            © Node Hill 2020
+          <div class="col text-center text light">
+           <button type="button" class="btn btn-link" click="backToTop">  <!-- link-type button that calls for 'backToTop' function -->
+              [Back to Top]          <!-- '[Back To Top]' receives the button link
+           </button>                                                      <!-- button ends -->
+          </div>
+        </div> 
+
+        <div class="row">
+         <div class="col text-center text-light">
+            © DhyrRumson
           </div>
         </div>
       </footer>
