5a297e119cc2ee324e04101d5f7ebdc23532795b
DouglasKarlberg <DouglasKarlberg@protonmail.com>
2020-03-03 17:31:26 +0100

    Added card with paragraphes about us and an image

--- www/js/OmOssPage.js
+++ www/js/OmOssPage.js
@@ -3,11 +3,32 @@ class OmOssPage extends Base {
   render() {
     return /*html*/`
       <div class="row" route="/om-oss" page-title="Om oss">
-        <div class="col-12">
-          <h1>Om oss</h1>
-          <p>Detta är en sida om oss.[UNDER UTVECKLING]</p>
-        </div>
+        <main>
+           <div class="card mb-3">
+              <img src="images/ExampleOmOss1.jpg" class="card-img-top" alt="...">
+                 <div class="card-body">
+                   <h5 class="text-center">Vilka är vi</h5>
+                   <p class="text-center">
+                   DhyrRumson är en av Sveriges snabbast växande mäklarföretag idag.
+                   Sedan början på 2020, har vi växt och spridit vårat nystartade privatföretag
+                   över nästan hela Sverige med inga tecken på att stoppa. I dagsläget befinner sig
+                   våran verksamhet bara i Sverige, men vi har planer runt hörnet; att sprida våran verksamhet
+                   ut över hela Skandinavien och så småningom; Europa.
+                   </p>
+                   <h4 class="text-center"> ______ </h4>
+                   <h5 class="text-center"> Våra partners </h5>
+                   <p class="text-center">
+                   Vi på DhyrRumson har en rad olika samarbetspartners för att hjälpa dig att göra en
+                   trygg och smidig bostadsaffär. På så sätt kan vi erbjuda dig stöd och rådgivning
+                   inom alla områden som rör din affär. Vi samarbetar med de bästa inom respektive område
+                   på marknaden för att alltid kunna leverera högsta kvalitet till våra kunder.
+                   </p>
+                   <h4 class="text-center"> ______ </h4>
+            </div>
+           </div>
+        </main> 
       </div>
+      
     `;
   }
 
