291568409d1352d87614f041f23d6541c294b205
RevaZara <reva1zara1@hotmail.com>
2020-03-04 14:33:03 +0100

    Ändra  (till salu ) till ( sök bostad)

---
+++ www/js/SokBostadPage.js
@@ -0,0 +1,16 @@
+class SokBostadPage extends Base {
+
+  render() {
+    return /*html*/`
+      <div class="row" route="/sok-bostad" page-title="Sök bostad">
+        <main>
+           <div class="mb-3">
+              Detta är sök bostad sida
+            </div>
+        </main> 
+      </div>
+      
+    `;
+  }
+
+}
