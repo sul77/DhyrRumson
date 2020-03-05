068640f6645b6d22a8a9d50f5f0440149056207b
Ikhlas99 <ikhlas.najib@hotmail.se>
2020-03-05 23:12:00 +0100

    Har lagt till text till nyproduktion men den inte klar än.

--- www/js/NyProduktionPage.js
+++ www/js/NyProduktionPage.js
@@ -2,6 +2,7 @@ class NyProduktionPage extends Base {
 
   mount() {
     this.carousel = new Carousel();
+
   }
 
   render() {
@@ -9,6 +10,31 @@ class NyProduktionPage extends Base {
       <div class="row" route="/Ny-produktion" page-title="Vi kan Nyproduktion">
       <div class="col-12">   
         ${this.carousel} 
+        
+
+        <div class="row">
+    <div class="col-sm-6">
+    <div class="card">
+      <div class="card-body">
+        <p class="font-weight-bold">  Letar du bostad ? </p>
+
+        <p class="card-text">Vi kan ny produktion <br>
+                             Här hittar du alla våra aktuella projekt.</p>
+        <a href="#" class="btn btn-primary">Läs mer </a>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-6">
+    <div class="card">
+      <div class="card-body">
+        <p class="font-weight-bold">  Bygger du bostad ? </p>
+        <p class="card-text">Vår ambition är att våra mäklare ska inleda ett samarbete med byggherrarna i ett tydigt skede
+                              för att skapa rätt föruttsättningar för varje projekt.</p>
+        <a href="#" class="btn btn-primary">Läs mer </a>
+      </div>
+    </div>
+  </div>
+</div>
       </div>
       </div>
     `;
