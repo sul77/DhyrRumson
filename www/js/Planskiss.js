bca73fffbbfa9c46eaedb884c9ca69916dc385d7
sul77 <mohsul01@gmail.com>
2020-03-07 00:45:06 +0100

    Fixade detalj planskiss sidan

---
+++ www/js/Planskiss.js
@@ -0,0 +1,16 @@
+class Planskiss extends Base {
+
+    render() {
+        return /*html*/ `
+      <div class="row" route="/Planskiss" page-title="Planskiss">
+        <main>
+        <h2 class="text-center">Planskiss</h2>
+           <div class="card mb-3">
+              <img src="images/planskiss1.jpg" class="card-img-top" alt="...">
+             
+           </div>
+        </main> 
+    `
+    }
+
+}
