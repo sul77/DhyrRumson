2a0e0f64316ff77567554f7cc4a19c39bc936c39
Ikhlas99 <ikhlas.najib@hotmail.se>
2020-03-06 15:59:22 +0100

    Förändring i Nyproduktion sida,sidan är nästan klart.Har lagt till en ny kolumn idatabas som heter projectName.

---
+++ www/js/byggHerrePage.js
@@ -0,0 +1,16 @@
+class ByggHerrePage extends Base {
+
+  render() {
+    return /*html*/`
+      <div class="row" route="/bygg-herre" page-title="Vi kan nyproduktion">
+        
+           <div class="mb-3">
+              Senast inom Nyproduktion
+            </div>
+        
+      </div>
+      
+    `;
+  }
+
+}
