00178cc28220e9019b8a0d6ac2680baa5afe1e43
Ikhlas99 <ikhlas.najib@hotmail.se>
2020-03-08 21:01:02 +0100

    gamla uppdaeringar

--- www/js/App.js
+++ www/js/App.js
@@ -1,47 +1,35 @@
 class App extends Base {
 
-    async mount() {
+  async mount() {
 
-        await this.sqlQueries();
-        this.navBarLinks = [{
-                label: 'Hem',
-                route: '/'
-            },
-            {
-                label: 'Sök bostad',
-                route: '/sok-bostad'
-            },
-            {
-                label: 'Köpa bostad',
-                route: '/kopa-bostad'
-            },
-            {
-                label: 'Sälja bostad',
-                route: '/salja-bostad'
-            },
-            {
-                label: 'Nyproduktion',
-                route: '/Ny-produktion'
-            },
-            {
-                label: 'Våra mäklare',
-                route: '/vara-maklare'
-            },
-            {
-                label: 'Kontakta oss',
-                route: '/Kontakta-Oss'
-            },
-            {
-                label: 'Planskiss',
-                route: '/Planskiss'
-            },
-            {
-                label: 'Om oss',
-                route: '/Om-Oss'
-            }
+    await this.sqlQueries();
+    this.navBarLinks = [{
+      label: 'Hem', route: '/'
+    },
+
+    { label: 'Sök bostad', route: '/sok-bostad' },
 
 
-<<<<<<< HEAD
+    { label: 'Köpa bostad', route: '/kopa-bostad' },
+
+
+    { label: 'Sälja bostad', route: '/salja-bostad' },
+
+
+    { label: 'Nyproduktion', route: '/Ny-produktion' },
+
+    { label: 'Våra mäklare', route: '/vara-maklare' },
+
+
+    { label: 'Kontakta oss', route: '/Kontakta-Oss' },
+
+
+    { label: 'Planskiss', route: '/Planskiss' },
+
+
+    { label: 'Om oss', route: '/Om-Oss' }
+
+
     ];
     this.navBar = new NavBar({ links: this.navBarLinks });
     this.footer = new Footer();
@@ -60,34 +48,15 @@ class App extends Base {
   async sqlQueries() {
     // Which database to use
     await sql(/*sql*/`
-=======
-        ];
-        this.navBar = new NavBar({
-            links: this.navBarLinks
-        });
-        this.footer = new Footer();
-        this.startPage = new StartPage();
-        this.OmOssPage = new OmOssPage();
-        this.KontaktaOssPage = new KontaktaOssPage();
-        this.missingPage = new MissingPage();
-        this.SokBostadPage = new SokBostadPage();
-        this.SaljaBostadPage = new SaljaBostadPage();
-        this.VaraMaklarePage = new VaraMaklarePage();
-        this.NyProduktionPage = new NyProduktionPage();
-        this.KopaBostadPage = new KopaBostadPage();
-        this.nyBostad = new NyBostadPage();
-        this.Planskiss = new Planskiss();
-    }
-    async sqlQueries() {
-        // Which database to use
-        await sql( /*sql*/ `
->>>>>>> a2c7e93bbdddc02bbd9591a66b4f08220559630f
       USE dhyrRumson
     `);
-    }
+  }
 
-    render() {
-        return /*html*/ `
+
+
+
+  render() {
+    return /*html*/ `
     
       <div style="color:'white'" base-title="Minimal: ">
         <header>
@@ -104,11 +73,11 @@ class App extends Base {
           ${this.NyProduktionPage}
           ${this.KopaBostadPage}
           ${this.nyBostad}
-<<<<<<< HEAD
+
           ${this.byggHerre}
-=======
+
           ${this.Planskiss}
->>>>>>> a2c7e93bbdddc02bbd9591a66b4f08220559630f
+
           
         </main>
 
@@ -116,6 +85,6 @@ class App extends Base {
         ${this.shoppingCart}
       </div>
     `;
-    }
+  }
 
 }
