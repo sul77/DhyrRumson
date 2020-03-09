258567382595651a065a40c63914af998437ce62
sul77 <mohsul01@gmail.com>
2020-03-10 00:52:09 +0100

    Sök bostad samt nya bilder och även ny data i databas

--- www/js/KopaBostadPage.js
+++ www/js/KopaBostadPage.js
@@ -1,11 +1,13 @@
 class KopaBostadPage extends Base {
-    mount(){
-        this.f = new ContactForm({toSave:"buy"});
+    mount() {
+        this.f = new ContactForm({
+            toSave: "buy"
+        });
     }
     render() {
-      return /*html*/`
+        return /*html*/ `
       <div class="row" route="/kopa-bostad" page-title="Köpa bostad">
-          <img src="images/ExampleBuyHouse4.jpg" height="350" class="card-img-top" alt="...">
+          <img src="images/ExampleBuyHouse.jpg" height="350" class="card-img-top" alt="...">
           <div class="col-12">
               <h3 class="mt-5 mb-3">Boka värdering av din bostad</h3>
               <p>Oavsett om du vill sälja nu, eller bara vill veta vad din bostad är värd, så hjälper vi dig gärna att värdera din bostad.</p>
@@ -14,4 +16,3 @@ class KopaBostadPage extends Base {
       </div>`;
     }
 }
-
--- www/js/SokBostadPage.js
+++ www/js/SokBostadPage.js
@@ -1,12 +1,53 @@
 class SokBostadPage extends Base {
-  render() {
-    return /*html*/`
-      <div class="row" route="/sok-bostad" page-title="Sök bostad">
-        <main>
-           <div class="mb-3">
+
+    async mount() {
+        this.housing = await sql( /*sql*/ `
+       SELECT Housing.*, Address.postalArea, Address.city,
+         GROUP_CONCAT(HousingImages.url) AS imageUrls
+       FROM Housing, HousingImages, Address
+       WHERE Housing.id = HousingImages.housingId
+       AND Housing.addressId = Address.id
+       
+       GROUP BY Housing.id
+    `);
+
+        // convert imageUrls to an array
+        for (let house of this.housing) {
+            house.imageUrls = house.imageUrls.split(',');
+        }
+
+        console.log(this.housing)
+    }
+
+    render() {
+            return /*html*/ `
+      <div route="/sok-bostad" page-title="Sök Bostad">
+      <h1>Sök resultat</h1>
+      ${this.housing.map(house => `
+      
+          <div class="row mb-5">
+          <div class="col-md-4">
+              <img src="${house.imageUrls[0]}" class="img-fluid">
+           </div>
+           
+            <div class="col-md-6 Sokbostad-facts">
+              <h3>${house.projectName}</h3>
+              <div class="Sokbostad-line"></div>
               
-            </div>
-        </main> 
+              <p>${house.description}</p>
+              <p><strong>Pris:</strong>${house.price} kr</p>
+              <p><strong>Antal Rum:</strong>${house.totalRooms} RoK</p>
+              <p><strong>Boarea:</strong>${house.livingArea} Kvm</p>
+              <p><strong>Område:</strong>${house.postalArea}</p>
+              <p><strong>Kommun:</strong>${house.city}</p>
+              <div class="col-md-20"><hr></div>
+              
+           </div>
+           
+          </div>
+      `)}
+
+        
       </div>
       
     `;
