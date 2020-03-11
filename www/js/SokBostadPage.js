7a4a7b6254436a52e080f7c9551eeda209b4bf82
Ammara Khan <ammarakhan63@yahoo.com>
2020-03-11 22:33:49 +0100

    Fixed wrong method name

--- www/js/SokBostadPage.js
+++ www/js/SokBostadPage.js
@@ -1,7 +1,7 @@
 class SokBostadPage extends Base {
 
   async mount() {
-    let url = decodeUri(window.location.search) // Getting URL and decoding it
+    let url = decodeURI(window.location.search) // Getting URL and decoding it
     console.log(url)
 
     let city = url.replace("?", ''); // Extracting city from query params
