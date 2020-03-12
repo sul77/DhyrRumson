f0ad1da18063a2904b828b2a91536b2dc2255863
DouglasKarlberg <DouglasKarlberg@protonmail.com>
2020-03-12 20:36:20 +0100

    merge

--- www/js/VaraMaklare.js
+++ www/js/VaraMaklare.js
@@ -18,6 +18,9 @@ class VaraMaklarePage extends Base {
       <div class="row" route="/vara-maklare" page-title="Våra mäklare">
 
        ${this.brokers.map(broker => `
+      
+
+ 
 
             <div class="col-md-4 mt-4 border">
               <div clss="col-md-12">
@@ -25,9 +28,9 @@ class VaraMaklarePage extends Base {
                   <img src="${broker.imgSrc}" style="width:150px">
                 </div>
                 <div clss="col-md-6" style="display: inline-block;">
-                  <p><strong> ${broker.firstName} ${broker.lastName}</strong></p>
-                  <p>${broker.email}</p>
-                  <p>${broker.telephone}</p>
+                  <h5><strong> ${broker.firstName} ${broker.lastName}</strong></h5>
+                  <h5><strong>Email: </strong>${broker.email}</h5>
+                  <h5><strong>Telephone: </strong>${broker.telephone}</h5>
               </div>
               </div>
               <div clss="col-md-12 mt-2">${broker.description}</div>
