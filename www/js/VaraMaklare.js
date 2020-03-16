17ee455db714a1fd329527929735539146d2db43
RevaZara <reva1zara1@hotmail.com>
2020-03-16 23:08:47 +0100

    Länka varje mäklare till epost

--- www/js/VaraMaklare.js
+++ www/js/VaraMaklare.js
@@ -12,18 +12,20 @@ class VaraMaklarePage extends Base {
       <div class="row" route="/vara-maklare" page-title="Våra mäklare">
 
        ${this.brokers.map(broker => `
-            <div class="col-md-6 shadow-lg p-3 bg-white rounded">
+            <div class="col-md-6 shadow-lg p-3 bg-white rounded" style="font-size: 17px;">
               <div class="col-md-12">
                 <div class="col-md-3">
                   <img src="${broker.imgSrc}" style="width:130px">
                 </div>
                 <div class="col-md-6 mt-5 ml-5">
-                  <h5><strong> ${broker.firstName} ${broker.lastName}</strong></h5>
-                  <h5><strong>Email: </strong>${broker.email}</h5>
-                  <h5><strong>Telephone: </strong>${broker.telephone}</h5>
+                  <p><strong> ${broker.firstName} ${broker.lastName}</strong></p>
+                  <p><strong>Email: </strong>
+                   <a href="mailto:${broker.email}">${broker.email}</a>
+                  </p>
+                  <p><strong>Telephone: </strong>${broker.telephone}</p>
                </div>
               </div>
-              <div class="col-md-12 mt-5 pb-5 text-justify">${broker.description}</div>
+              <div class="col-md-12 mt-5 pb-5">${broker.description}</div>
             </div>
       `)}
     </div>`;
