ba76df404af7f8a41999a200b1b5c02050ea5760
DouglasKarlberg <DouglasKarlberg@protonmail.com>
2020-03-17 20:21:35 +0100

    Changes in Nyproduktion incl. phone responsiveness

--- www/js/NyProduktionPage.js
+++ www/js/NyProduktionPage.js
@@ -18,28 +18,44 @@ class NyProduktionPage extends Base {
         
 
         <div class="row">
-    <div class="col-sm-6">
-    <div class="card">
-      <div class="card-body">
-        <p class="font-weight-bold">  Letar du bostad ? </p>
+            <div class="col-sm-6" style="margin-top: 20px;">
+            <div class="card">
+              <div class="card-body">
+                <p class="font-weight-bold">  Letar du bostad ? </p>
 
-        <p class="card-text">Vi kan ny produktion <br>
-                             Här hittar du alla våra aktuella projekt.</p>
-        <a href="/nya-bostader" class="btn btn-primary">Läs mer </a>
-      </div>
-    </div>
-  </div>
-  <div class="col-sm-6">
-    <div class="card">
-      <div class="card-body">
-        <p class="font-weight-bold">  Bygger du bostad ? </p>
-        <p class="card-text">Vår ambition är att våra mäklare ska inleda ett samarbete med byggherrarna i ett tydigt skede
-                              för att skapa rätt föruttsättningar för varje projekt.</p>
-        <a href="bygg-herre" class="btn btn-primary">Läs mer </a>
-      </div>
-    </div>
-  </div>
-</div>
+                <p class="card-text">Vi kan ny produktion <br>
+                                    Här hittar du alla våra aktuella projekt.</p>
+                <a href="/nya-bostader" class="btn btn-dark btn-md sharp" style="background: #5a5a5a;">Läs mer </a>
+              </div>
+            </div>
+          </div>
+          <div class="col-sm-6" style="margin-top: 20px;">
+            <div class="card">
+              <div class="card-body">
+                <p class="font-weight-bold">  Bygger du bostad ? </p>
+                <p class="card-text">Vår ambition är att våra mäklare ska inleda ett samarbete med byggherrarna i ett tydligt skede
+                                      för att skapa rätt föruttsättningar för varje projekt.</p>
+                <a href="bygg-herre" class="btn btn-dark btn-md sharp" style="background: #5a5a5a;">Läs mer </a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="card mb-3" style="margin-top: 20px;">
+        <div class="row">
+        <div class="col-xs-12 hidden-sm hidden-md hidden-lg">  
+        <img class="img-responsive" src="/images/DhyrRumsonWallpaperZoomedPhone.jpg" />    <!--Mobile-->
+        </div>
+        <div class="hidden-xs col-sm-12 hidden-md hidden-lg">
+            <img class="img-responsive" src="/images/DhyrRumsonWallpaperZoomed.jpg" />   <!--Tab-->
+        </div>
+        <div class="hidden-xs hidden-sm col-md-12 hidden-lg">
+            <img class="img-responsive" src="/images/DhyrRumsonWallpaper.jpg" />   <!--Desktop-->
+        </div>
+        <div class="hidden-xs hidden-sm hidden-md col-lg-12">
+            <img class="img-responsive" src="/images/DhyrRumsonWallpaper.jpg" />  <!--Large-->
+        </div>
+        </div>
+        </div>
       </div>
       </div>
     `;
