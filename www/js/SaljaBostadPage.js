ef5915165a43ed25d5d6e59251405dd917ee9265
DouglasKarlberg <DouglasKarlberg@protonmail.com>
2020-03-19 14:57:49 +0100

    Gave cards for S..BostadPage and K.BostadPage+more

--- www/js/SaljaBostadPage.js
+++ www/js/SaljaBostadPage.js
@@ -5,12 +5,16 @@ class SaljaBostadPage extends Base {
     render() {
         return /*html*/`
       <div class="row" route="/salja-bostad" page-title="Sälja bostad">
-          <img src="images/ExampleSaleHouse.jpg" height="350" class="card-img-top saljbostad-img" alt="...">
-          <div class="col-12">
-              <h3 class="mt-5 mb-3">Boka värdering av din bostad</h3>
-              <p>Oavsett om du vill sälja nu, eller bara vill veta vad din bostad är värd, så hjälper vi dig gärna att värdera din bostad.</p>
-              ${this.f}
-          </div>
+        <div class="card mb-3">
+                <img src="images/ExampleSellHouse.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+                 <div class="col-12">
+                     <h3 class="mt-5 mb-3">Boka värdering av din bostad</h3>
+                     <p>Oavsett om du vill sälja nu, eller bara vill veta vad din bostad är värd, så hjälper vi dig gärna att värdera din bostad.</p>
+                     ${this.f}
+                 </div>
+            </div>     
+        </div>         
       </div>`;
     }
 }
