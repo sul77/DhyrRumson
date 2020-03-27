116043ac863dfd356a9a6f50d8e650d93c40ec67
Ammara Khan <ammarakhan63@yahoo.com>
2020-03-27 09:40:46 +0100

    Resolved merge conflicts Removed Undefined class instance

--- www/js/App.js
+++ www/js/App.js
@@ -1,73 +1,71 @@
 class App extends Base {
 
-    async mount() {
+  async mount() {
 
-        await this.sqlQueries();
-        this.navBarLinks = [{
-                label: 'Hem',
-                route: '/'
-            },
-            {
-                label: 'Sök bostad',
-                route: '/sok-bostad'
-            },
-            {
-                label: 'Köpa bostad',
-                route: '/kopa-bostad'
-            },
-            {
-                label: 'Sälja bostad',
-                route: '/salja-bostad'
-            },
-            {
-                label: 'Nyproduktion',
-                route: '/Ny-produktion'
-            },
-            {
-                label: 'Våra mäklare',
-                route: '/vara-maklare'
-            },
-            {
-                label: 'Om oss',
-                route: '/Om-Oss'
-            },
-            {
-                label: 'Kontakta oss',
-                route: '/Kontakta-Oss'
-            }
-
-        ];
-
-
-        this.navBar = new NavBar({
-            links: this.navBarLinks
-        });
-        this.footer = new Footer();
-        this.startPage = new StartPage();
-        this.omOssPage = new OmOssPage();
-        this.kontaktaOssPage = new KontaktaOssPage();
-        this.missingPage = new MissingPage();
-        this.sokBostadPage = new SokBostadPage();
-        this.saljaBostadPage = new SaljaBostadPage();
-        this.varaMaklarePage = new VaraMaklarePage();
-        this.nyProduktionPage = new NyProduktionPage();
-        this.kopaBostadPage = new KopaBostadPage();
-        this.nyBostad = new NyBostadPage();
-        this.byggHerre = new ByggHerrePage();
-        this.planskiss = new Planskiss();
-        this.imagePlanskiss = new ImagePlanskiss();
-        this.integritetPolicyPage = new IntegritetPolicyPage();
+    await this.sqlQueries();
+    this.navBarLinks = [{
+      label: 'Hem',
+      route: '/'
+    },
+    {
+      label: 'Sök bostad',
+      route: '/sok-bostad'
+    },
+    {
+      label: 'Köpa bostad',
+      route: '/kopa-bostad'
+    },
+    {
+      label: 'Sälja bostad',
+      route: '/salja-bostad'
+    },
+    {
+      label: 'Nyproduktion',
+      route: '/Ny-produktion'
+    },
+    {
+      label: 'Våra mäklare',
+      route: '/vara-maklare'
+    },
+    {
+      label: 'Om oss',
+      route: '/Om-Oss'
+    },
+    {
+      label: 'Kontakta oss',
+      route: '/Kontakta-Oss'
     }
 
-    async sqlQueries() {
-        // Which database to use
-        await sql( /*sql*/ `
+    ];
+
+
+    this.navBar = new NavBar({
+      links: this.navBarLinks
+    });
+    this.footer = new Footer();
+    this.startPage = new StartPage();
+    this.omOssPage = new OmOssPage();
+    this.kontaktaOssPage = new KontaktaOssPage();
+    this.missingPage = new MissingPage();
+    this.sokBostadPage = new SokBostadPage();
+    this.saljaBostadPage = new SaljaBostadPage();
+    this.varaMaklarePage = new VaraMaklarePage();
+    this.nyProduktionPage = new NyProduktionPage();
+    this.kopaBostadPage = new KopaBostadPage();
+    this.nyBostad = new NyBostadPage();
+    this.byggHerre = new ByggHerrePage();
+    this.planskiss = new Planskiss();
+    this.integritetPolicyPage = new IntegritetPolicyPage();
+  }
+
+  async sqlQueries() {
+    // Which database to use
+    await sql( /*sql*/ `
       USE dhyrRumson
     `);
 
-<<<<<<< HEAD
     // DEN HÄR KODEN HADE FÖRSVUNNIT HELT!!!
-    this.bostadPages = await sql(BostadPage, /*sql*/`
+    this.bostadPages = await sql(BostadPage, /*sql*/ `
       SELECT * FROM Housing
     `);
 
@@ -92,37 +90,9 @@ class App extends Base {
     }
     return x;
   }
-=======
-        // DEN HÄR KODEN HADE FÖRSVUNNIT HELT!!!
-        this.bostadPages = await sql(BostadPage, /*sql*/ `
-      SELECT * FROM Housing
-    `);
 
-        // Create nyBostad pages from a db query
-        this.nyDetaljPages = await sql(NyDetaljPage, /*sql*/ `
-      SELECT * FROM nyDetaljInfo
-    `);
-    }
-
-    cleanName(x) {
-        // remove å, ä, ö in names that will be used in urls
-        let toReplace = {
-            'å': 'a',
-            'ä': 'a',
-            'ö': 'o',
-            'Å': 'å',
-            'Ä': 'ä',
-            'Ö': 'O',
-        };
-        for (let [key, val] of Object.entries(toReplace)) {
-            x = x.split(key).join(val);
-        }
-        return x;
-    }
->>>>>>> 7a65ea5cb8b4366dbad7e2e58e9b237a850b8646
-
-    render() {
-        return /*html*/ `
+  render() {
+    return /*html*/ `
     
       <div style="color:'white'" base-title="Minimal: ">
         <header>
@@ -144,12 +114,11 @@ class App extends Base {
           ${this.byggHerre}
           ${this.planskiss}
           ${this.nyDetaljPages}
-          ${this.ImagePlanskiss}
         </main>
         ${this.footer}
         ${this.shoppingCart}
       </div>
     `;
-    }
+  }
 
 }
