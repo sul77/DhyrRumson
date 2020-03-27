6d494f182435607b82b12b94d000f5341e0c4e42
sul77 <mohsul01@gmail.com>
2020-03-27 11:05:16 +0100

    fix in app.js

--- www/js/App.js
+++ www/js/App.js
@@ -1,112 +1,106 @@
 class App extends Base {
 
-  async mount() {
+    async mount() {
 
-    await this.sqlQueries();
-    this.navBarLinks = [{
-      label: 'Hem',
-      route: '/'
-    },
-    {
-      label: 'Sök bostad',
-      route: '/sok-bostad'
-    },
-    {
-      label: 'Köpa bostad',
-      route: '/kopa-bostad'
-    },
-    {
-      label: 'Sälja bostad',
-      route: '/salja-bostad'
-    },
-    {
-      label: 'Nyproduktion',
-      route: '/Ny-produktion'
-    },
-    {
-      label: 'Våra mäklare',
-      route: '/vara-maklare'
-    },
-    {
-      label: 'Om oss',
-      route: '/Om-Oss'
-    },
-    {
-      label: 'Kontakta oss',
-      route: '/Kontakta-Oss'
+        await this.sqlQueries();
+        this.navBarLinks = [{
+                label: 'Hem',
+                route: '/'
+            },
+            {
+                label: 'Sök bostad',
+                route: '/sok-bostad'
+            },
+            {
+                label: 'Köpa bostad',
+                route: '/kopa-bostad'
+            },
+            {
+                label: 'Sälja bostad',
+                route: '/salja-bostad'
+            },
+            {
+                label: 'Nyproduktion',
+                route: '/Ny-produktion'
+            },
+            {
+                label: 'Våra mäklare',
+                route: '/vara-maklare'
+            },
+            {
+                label: 'Om oss',
+                route: '/Om-Oss'
+            },
+            {
+                label: 'Kontakta oss',
+                route: '/Kontakta-Oss'
+            }
+
+        ];
+
+
+        this.navBar = new NavBar({
+            links: this.navBarLinks
+        });
+        this.footer = new Footer();
+        this.startPage = new StartPage();
+        this.omOssPage = new OmOssPage();
+        this.kontaktaOssPage = new KontaktaOssPage();
+        this.missingPage = new MissingPage();
+        this.sokBostadPage = new SokBostadPage();
+        this.saljaBostadPage = new SaljaBostadPage();
+        this.varaMaklarePage = new VaraMaklarePage();
+        this.nyProduktionPage = new NyProduktionPage();
+        this.kopaBostadPage = new KopaBostadPage();
+        this.nyBostad = new NyBostadPage();
+        this.byggHerre = new ByggHerrePage();
+        this.planskiss = new Planskiss();
+        this.integritetPolicyPage = new IntegritetPolicyPage();
     }
 
-    ];
-
-
-    this.navBar = new NavBar({
-      links: this.navBarLinks
-    });
-    this.footer = new Footer();
-    this.startPage = new StartPage();
-    this.omOssPage = new OmOssPage();
-    this.kontaktaOssPage = new KontaktaOssPage();
-    this.missingPage = new MissingPage();
-    this.sokBostadPage = new SokBostadPage();
-    this.saljaBostadPage = new SaljaBostadPage();
-    this.varaMaklarePage = new VaraMaklarePage();
-    this.nyProduktionPage = new NyProduktionPage();
-    this.kopaBostadPage = new KopaBostadPage();
-    this.nyBostad = new NyBostadPage();
-    this.byggHerre = new ByggHerrePage();
-    this.planskiss = new Planskiss();
-    this.integritetPolicyPage = new IntegritetPolicyPage();
-  }
-
-  async sqlQueries() {
-    // Which database to use
-    await sql( /*sql*/ `
+    async sqlQueries() {
+        // Which database to use
+        await sql( /*sql*/ `
       USE dhyrRumson
     `);
 
-<<<<<<< HEAD
-    this.bostadPages = await sql(BostadPage, /*sql*/ `
+        this.bostadPages = await sql(BostadPage, /*sql*/ `
       SELECT * FROM detailSearchResult
-=======
-    // DEN HÄR KODEN HADE FÖRSVUNNIT HELT!!!
-    this.bostadPages = await sql(BostadPage, /*sql*/ `
-      SELECT * FROM Housing
->>>>>>> a55a5dd1abf55e59940776f683d615aed1b6fa7a
     `);
-    console.log(this.bostadPages);
+        console.log(this.bostadPages);
 
-    // Create nyBostad pages from a db query
-    this.nyDetaljPages = await sql(NyDetaljPage, /*sql*/ `
+        // Create nyBostad pages from a db query
+        this.nyDetaljPages = await sql(NyDetaljPage, /*sql*/ `
       SELECT * FROM nyDetaljInfo
     `);
-  }
-
-  cleanName(x) {
-    // remove å, ä, ö in names that will be used in urls
-    let toReplace = {
-      'å': 'a',
-      'ä': 'a',
-      'ö': 'o',
-      'Å': 'å',
-      'Ä': 'ä',
-      'Ö': 'O',
-    };
-    for (let [key, val] of Object.entries(toReplace)) {
-      x = x.split(key).join(val);
     }
-    return x;
-  }
 
-<<<<<<< HEAD
-  formatCurrency(x) {
-    let formatter = new Intl.NumberFormat('se-SE', { style: 'currency', currency: 'SEK' });
-    return formatter.format(x).split(',00').join('');
-  }
+    cleanName(x) {
+        // remove å, ä, ö in names that will be used in urls
+        let toReplace = {
+            'å': 'a',
+            'ä': 'a',
+            'ö': 'o',
+            'Å': 'å',
+            'Ä': 'ä',
+            'Ö': 'O',
+        };
+        for (let [key, val] of Object.entries(toReplace)) {
+            x = x.split(key).join(val);
+        }
+        return x;
+    }
 
-=======
->>>>>>> a55a5dd1abf55e59940776f683d615aed1b6fa7a
-  render() {
-    return /*html*/ `
+    formatCurrency(x) {
+        let formatter = new Intl.NumberFormat('se-SE', {
+            style: 'currency',
+            currency: 'SEK'
+        });
+        return formatter.format(x).split(',00').join('');
+    }
+
+    render() {
+        return /*html*/ `
     
       <div style="color:'white'" base-title="Minimal: ">
         <header>
@@ -133,6 +127,6 @@ class App extends Base {
         ${this.shoppingCart}
       </div>
     `;
-  }
+    }
 
 }
